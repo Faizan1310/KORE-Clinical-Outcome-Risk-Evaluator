11b3713d6f98eca3ef36c0c01655b8de10e9f2c0
Faizan Khan <faizan23khan0312@email.com>
2026-03-29 03:19:06 +0530

    Fixed db.create_all to run on startup

--- webapp/app.py
+++ webapp/app.py
@@ -377,9 +377,9 @@ def admin():
                            total_predictions=total_predictions,
                            high_risk=high_risk) 
 
-if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        
 if __name__ == '__main__':
     app.run(debug=True)
     
