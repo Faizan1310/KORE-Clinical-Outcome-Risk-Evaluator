303f84c2730f59c3b24d1b0cf721e05ef29e064a
Faizan Khan <faizan23khan0312@email.com>
2026-03-29 03:26:01 +0530

    Fixed db creation using before_request

--- webapp/app.py
+++ webapp/app.py
@@ -377,9 +377,10 @@ def admin():
                            total_predictions=total_predictions,
                            high_risk=high_risk) 
 
-    with app.app_context():
-        db.create_all()
-        
+@app.before_request
+def create_tables():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
     
